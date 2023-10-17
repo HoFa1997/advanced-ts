19f5dca46989defbb0497de0a319ee0b52cad457
Hossein Fatemi <hfatemi1997@gmail.com>
2023-10-17 23:32:25 +0330

    add 01

--- src/01-branded-types/01-whatIsBrandedTypes.ts
+++ src/01-branded-types/01-whatIsBrandedTypes.ts
@@ -3,8 +3,21 @@ import { Brand } from "../helper/Brand";
 type Password = Brand<string, "Password">;
 type Email = Brand<string, "Email">;
 
+type UserObject = Brand<{ id: string; name: string }, "User">;
+
+const user: UserObject = {
+  id: "asdasd",
+  name: "asdasd",
+} as UserObject;
+
 const password = "1231423" as Password;
 
 const email = "mpocock@me.com" as Email;
 
+const verifyPassword = (pass: Password) => {};
+
+verifyPassword("asdasdas" as Password);
+
 let passwordSlot: Password;
+
+passwordSlot = "asdasdasdas" as Password;
